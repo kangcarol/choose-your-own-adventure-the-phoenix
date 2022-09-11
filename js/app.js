227b5b9200fe07036fcd8290720d7dfe7691e09d
Carol Kang <otherhats@gmail.com>
2022-09-11 12:11:17 -0400

    fix so that when going to StoryOver the feather disappears from image area

--- js/app.js
+++ js/app.js
@@ -49,7 +49,7 @@ const environmentPhoenix = { //this is environment to find phoenix
   const featherBox = document.getElementById('feather-box')
   const timerArea = document.getElementById('timer-area')
 
-  // const mainImg = document.getElementById('main-img')
+  const mainImg = document.querySelector('.locationCells')
 
   /*----------------------------- Event Listeners -----------------------------*/
 
@@ -67,85 +67,87 @@ const environmentPhoenix = { //this is environment to find phoenix
   init()
 
   function init (){
-  featherTotal = 0
-  enviroGrid = [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null]
-  //todo refactor enviroGrid later, and the id's for cellEle's
+    featherTotal = 0
+    enviroGrid = [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null]
 
-  console.log('INIT',`feather total = ${featherTotal}`)
+    console.log('INIT',`feather total = ${featherTotal}`)
 
-  // Upon loading page, it should show image of phoenix and intro message
-  // IMAGE -> Phoenix intro drawing
-  // MESSAGE -> intro about phoenix mythology & how the game works
-  // BUTTONS -> only START button should be visible, hide all others
-  // FEATHER-BOX -> hidden
-  // TIMER -> hidden
-  startBtn.removeAttribute('hidden')
-  resetBtn.setAttribute('hidden', true)
-  choice1Btn.setAttribute('hidden', true)
-  choice2Btn.setAttribute('hidden', true)
-  featherBox.setAttribute('hidden', true)
-  timerArea.setAttribute('hidden', true)
+    // Upon loading page, it should show image of phoenix and intro message
+    // IMAGE -> Phoenix intro drawing
+    // MESSAGE -> intro about phoenix mythology & how the game works
+    // BUTTONS -> only START button should be visible, hide all others
+    // FEATHER-BOX -> hidden
+    // TIMER -> hidden
+    startBtn.removeAttribute('hidden')
+    resetBtn.setAttribute('hidden', true)
+    choice1Btn.setAttribute('hidden', true)
+    choice2Btn.setAttribute('hidden', true)
+    featherBox.setAttribute('hidden', true)
+    timerArea.setAttribute('hidden', true)
 
-  // CLEAR PREVIOUSLY FOUND FEATHERS
-  enviroGrid.forEach((cell,i) => {
-    if (cellEls[i].textContent = 'FEATHER'){
-      cellEls[i].textContent = null
-    }
-  })
+    // CLEAR PREVIOUSLY FOUND FEATHERS
+    enviroGrid.forEach((cell,i) => {
+      if (cellEls[i].textContent = 'FEATHER'){
+        cellEls[i].textContent = null
+      }
+    })
 
-}
+    // REVERT TO INTRO IMAGE
+    mainImg.style.backgroundImage = "url('../images/catherdral.jpg')"
 
-function handleClickStart(evt){
-  renderEnvironment()
-  //todo START TIMER
-  console.log("START", `feather total = ${featherTotal}`) //! delete later
-}
+  }
 
-function renderEnvironment(){
-  enviro = environment1 // to change out
-  enviroGrid[enviro.secretLocation] = 'feather'
-  //todo IMAGE -> change out to Environment-1 image
-  //todo MESSAGE -> change out to Environment-1 hint message
-  // BUTTONS -> hide START button, show all other buttons
-  // FEATHER-BOX -> show
-  // TIMER -> show
-  startBtn.setAttribute('hidden', true)
-  resetBtn.removeAttribute('hidden')
-  choice1Btn.removeAttribute('hidden')
-  choice2Btn.removeAttribute('hidden')
-  featherBox.removeAttribute('hidden')
-  timerArea.removeAttribute('hidden')
-}
+  function handleClickStart(evt){
+    renderEnvironment()
+    //todo START TIMER
+    console.log("START", `feather total = ${featherTotal}`) //! delete later
+  }
 
-function handleClickFind(evt){
-  // FEATHER
+  function renderEnvironment(){
+    enviro = environment1 // to change out
+    enviroGrid[enviro.secretLocation] = 'feather'
+    //todo IMAGE -> change out to Environment-1 image
+    //todo MESSAGE -> change out to Environment-1 hint message
+    // BUTTONS -> hide START button, show all other buttons
+    // FEATHER-BOX -> show
+    // TIMER -> show
+    startBtn.setAttribute('hidden', true)
+    resetBtn.removeAttribute('hidden')
+    choice1Btn.removeAttribute('hidden')
+    choice2Btn.removeAttribute('hidden')
+    featherBox.removeAttribute('hidden')
+    timerArea.removeAttribute('hidden')
+  }
+
+  function handleClickFind(evt){
+    // FEATHER
     // player finds cell (handleClickFind) that feather is assigned for THIS environment by clicking on cell.
     // when cell is clicked, the feather image will appear over image, and then it disappears and reappears in the feather box in next environment, and feather count increases
-  const sqIdx = parseInt(evt.target.id.replace('c', ''))
+    const sqIdx = parseInt(evt.target.id.replace('c', ''))
 
-  if (enviroGrid[sqIdx] === 'feather'){
-    let locationCell = cellEls[sqIdx]
-    locationCell.textContent = 'FEATHER'
-    locationCell.style.color = 'red'
-    //todo locationCell.className = 'animate__animated animate__ANIMATION-NAME'
-    featherTotal += 1
-    console.log('AFTER FIND feather total', `${featherTotal}`) //! delete later
-    enviroGrid = [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null]
-    // this is so that feather count does not continue to increase by clicking the same cell
-    //todo refactor enviroGrid later
-    //todo figure out how to loop each environment change
+    if (enviroGrid[sqIdx] === 'feather'){
+      let locationCell = cellEls[sqIdx]
+      locationCell.textContent = 'FEATHER'
+      locationCell.style.color = 'red'
+      //todo locationCell.className = 'animate__animated animate__ANIMATION-NAME'
+      featherTotal += 1
+      console.log('AFTER FIND feather total', `${featherTotal}`) //! delete later
+      enviroGrid = [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null]
+      //todo refactor enviroGrid later, and the id's for cellEle's
+      // this is so that feather count does not continue to increase by clicking the same cell
+      //todo figure out how to loop each environment change
   }
 }
 
   function handleClickChoice(evt){
- // CHOICE BUTTONS (MAth.random the buttons??)
+ //todo CHOICE BUTTONS (MAth.random the buttons??)
     // if player clicks on CHOICE button1 for Enviroment1, it will change renderEnvironment for Environment2
     // if player clicks on CHOICE button2, it will check if featherTotal > 0,
-      // if greater than 0, an alert pops up saying that they had chosen a fatal situation but their feather has granted them another life. After player clicks OK ("phew") it will take them to Environment2
+      // if greater than 0, an alert pops up saying that they had chosen a fatal situation but their feather has granted them another life. After player clicks OK it will take them to Environment2
       // if feather is less than 0, will take them to StoryOver
     if (evt.target.id === 'choice1-btn'){
       console.log('CHOICE 1')
-      environment = environment2
+      environment = environment2 //todo change out to new img/hint/secretLocation
     }else if (evt.target.id === 'choice2-btn'){
       console.log('CHOICE 2')
       if (featherTotal > 0){
@@ -153,6 +155,7 @@ function handleClickFind(evt){
         featherTotal -= 1
         //todo remove the visual feather from the feather box
         console.log('SPEND FEATHER feather total', `${featherTotal}`) //! delete later
+        renderStoryOver()
       } else{
         renderStoryOver()
       }
@@ -167,6 +170,13 @@ function handleClickFind(evt){
   // FEATHER-BOX -> show (????)
   // TIMER -> hide
     console.log("RENDER STORY OVER")
+    enviroGrid.forEach((cell,i) => {
+      if (cellEls[i].textContent = 'FEATHER'){
+        cellEls[i].textContent = null
+      }
+    })
+    mainImg.style.backgroundImage = "url('../images/forest.png')"
+
   }
 
   function renderConsolation(){
@@ -195,14 +205,13 @@ function handleClickFind(evt){
 
 /*-------------------------------- FRIDAY/WKEND GOALS --------------------------------*/
 
-  //todo Alert needed
-
-  //todo FIX so that at start cannot click to find feather or phoenix
-  //todo FIX so that feather count doesn't increase after getting 1 count
-
-  //todo ADD Hover
-
   //todo changing out environments as loop ??????
+  //todo ADD Hover
+  //todo ADD timer
+
+  //todo research phoenix mythology for images/sounds
+  //todo find images BLACK & WHITE LINE DRAWINGS ???
+
 
 
 /*------------------------------- NEXT WEEK ----------------------------------------*/

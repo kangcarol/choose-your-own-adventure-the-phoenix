ec5b29de61cc62ec1b580a322ea282306ed859f0
Carol Kang <otherhats@gmail.com>
2022-09-10 22:35:04 -0400

    add very basic css

--- js/app.js
+++ js/app.js
@@ -30,10 +30,10 @@ const environmentPhoenix = { //this is environment to find phoenix
   hint: 'hint for phoenix',
   secretLocation: 7,
 }
+
   /*-------------------------------- Variables --------------------------------*/
 
-  let featherTotal = 0
-  let environment, environmentGrid
+  let featherTotal, environment, enviroGrid
 
   /*------------------------ Cached Element References ------------------------*/
 
@@ -59,6 +59,9 @@ const environmentPhoenix = { //this is environment to find phoenix
     cellSelect.addEventListener('click', handleClickFind)
   })
 
+  choice1Btn.addEventListener('click', handleClickChoice)
+  choice2Btn.addEventListener('click', handleClickChoice)
+
   resetBtn.addEventListener('click', init) // maybe start elsewhere, further in?
 
   /*-------------------------------- Functions --------------------------------*/
@@ -66,8 +69,9 @@ const environmentPhoenix = { //this is environment to find phoenix
   init()
 
   function init (){
-
-  environmentGrid = [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null]
+  featherTotal = 0
+  enviroGrid = [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null]
+  //todo refactor enviroGrid later, and the id's for cellEle's
 
   console.log('INIT',`feather total = ${featherTotal}`)
 
@@ -85,25 +89,25 @@ const environmentPhoenix = { //this is environment to find phoenix
   timerArea.setAttribute('hidden', true)
 
   // CLEAR PREVIOUSLY FOUND FEATHERS
-  environmentGrid.forEach((cell,i) => {
+  enviroGrid.forEach((cell,i) => {
     if (cellEls[i].textContent = 'phx'){
       cellEls[i].textContent = null
     }
   })
-  featherTotal = 0
+
 }
 
 function handleClickStart(evt){
-  // let environment = environment1 // to change out
-  environmentGrid[environment1.secretLocation] = 'feather'
   renderEnvironment()
   //todo START TIMER
-  console.log("START", `feather total = ${featherTotal}`)
+  console.log("START", `feather total = ${featherTotal}`) //! delete later
 }
 
-function renderEnvironment(){
-  // IMAGE -> change out to Environment-1 image
-  // MESSAGE -> change out to Environment-1 hint message
+function renderEnvironment(enviro){
+  enviro = environment1 // to change out
+  enviroGrid[enviro.secretLocation] = 'feather'
+  //todo IMAGE -> change out to Environment-1 image
+  //todo MESSAGE -> change out to Environment-1 hint message
   // BUTTONS -> hide START button, show all other buttons
   // FEATHER-BOX -> show
   // TIMER -> show
@@ -120,19 +124,42 @@ function handleClickFind(evt){
     // player finds cell (handleClickFind) that feather is assigned for THIS environment by clicking on cell.
     // when cell is clicked, the feather image will appear over image, and then it disappears and reappears in the feather box in next environment, and feather count increases
   const sqIdx = parseInt(evt.target.id.replace('c', ''))
-  // if (environmentGrid[sqIdx])
-  if (environmentGrid[sqIdx] === 'feather'){
+
+  if (enviroGrid[sqIdx] === 'feather'){
     let locationCell = cellEls[sqIdx]
     locationCell.textContent = 'phx'
     locationCell.style.color = 'red'
     //todo locationCell.className = 'animate__animated animate__ANIMATION-NAME'
     featherTotal += 1
-    console.log('AFTER FIND feather total', `${featherTotal}`)
-    environmentGrid[sqIdx] === null
+    console.log('AFTER FIND feather total', `${featherTotal}`) //! delete later
+    enviroGrid = [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null]
+    // this is so that feather count does not continue to increase  by clicking the same cell
+    //todo refactor enviroGrid later
+    //todo figure out how to loop each environment change
   }
 }
 
-    // the location cell cannot be clicked again to add more than 1 feather during that environment
+  function handleClickChoice(evt){
+ // CHOICE BUTTONS (MAth.random the buttons??)
+    // if player clicks on CHOICE button1 for Enviroment1, it will change renderEnvironment for Environment2
+    // if player clicks on CHOICE button2, it will check if featherTotal > 0,
+      // if greater than 0, an alert pops up saying that they had chosen a fatal situation but their feather has granted them another life. After player clicks OK ("phew") it will take them to Environment2
+      // if feather is less than 0, will take them to StoryOver
+    if (evt.target.id === 'choice1-btn'){
+      console.log('CHOICE 1')
+      environment = environment2
+    }else if (evt.target.id === 'choice2-btn'){
+      console.log('CHOICE 2')
+      if (featherTotal > 0){
+        alert('You have chosen a FATAL scenerio, but you have a feather in your bank to grant you another life!')
+        featherTotal -= 1
+        //todo remove the visual feather from the feather box
+        console.log('SPEND FEATHER feather total', `${featherTotal}`) //! delete later
+      } else{
+        renderStoryOver()
+      }
+    }
+  }
 
 
   function renderStoryOver(){
@@ -141,6 +168,7 @@ function handleClickFind(evt){
   // BUTTONS -> hide all buttons, except reset
   // FEATHER-BOX -> show (????)
   // TIMER -> hide
+    console.log("RENDER STORY OVER")
   }
 
   function renderConsolation(){
@@ -163,59 +191,38 @@ function handleClickFind(evt){
 
   }
 
-  function handleClickChoice(evt){
- // CHOICE BUTTONS (MAth.random the buttons??)
-    // if player clicks on CHOICE button1 for Enviroment1, it will change renderEnvironment for Environment2
-    // if player clicks on CHOICE button2, it will check if featherTotal > 0,
-      // if greater than 0, an alert pops up saying that they had chosen a fatal situation but their feather has granted them another life. After player clicks OK ("phew") it will take them to Environment2
-      // if feather is less than 0, will take them to StoryOver
-
-  }
-
   function timer(){
   }
 
 
 /*-------------------------------- FRIDAY/WKEND GOALS --------------------------------*/
 
-  // Alert needed:
-    // resurrection alert
+  //todo Alert needed
 
-  // FIX so that at start cannot click to find feather or phoenix
-  // FIX so that feather count doesn't increase after getting 1 count
+  //todo FIX so that at start cannot click to find feather or phoenix
+  //todo FIX so that feather count doesn't increase after getting 1 count
 
-  // ADD Hover
+  //todo ADD Hover
 
-  // changing out environments as loop ??????
+  //todo changing out environments as loop ??????
 
 
 /*------------------------------- NEXT WEEK ----------------------------------------*/
 
 // Images & copywrite the hints
-// Timer
-// Sound
-// Favicon
-
 // Copy of messages/hints
+// TIMER
+
+// Font
+// Add HOVER feature
+// Add animation
+// Sound
+//! Make sure image doesn't get distored
+
 
 
 /*----------------------------------------------------------------------------------*/
 
-
-
-// START SCREEN:
-  // HTML will already have the img source loaded for the start screen.
-  // HTML will have a message box
-  // HTML will have a start button
-  // HTML will have the restart button hidden
-  // CSS to have an invisible grid for clicks (to find feather or phoenix)
-
-
-// SET UP:
-  //// Create cached element references
-  //// Let variable for total number of feathers (that will be increase/decrease as feathers are found or “spent”)
-  //// Create const variables for the locations of each of the feathers and phoenix
-  //// Grid array for possible locations (like tic tac toe)
 
 
 // AddListener function for the start button, render:
@@ -243,8 +250,3 @@ function handleClickFind(evt){
 
 // PHOENIX SCREEN (environment to look for phoenix)
 // When find phoenix, image of phoenix replaces screen with a congratulations
-
-
-// TIMER !!!!!
-
-// FAVICON
